bbc42d658f3a8d26a59fba3d003b21c812191ca6
mehdiz98 <mehdi_razavi97@yahoo.com>
2017-06-08 16:20:20 +0430

    Update tile_check.v

--- verilog_code/tile_check.v
+++ verilog_code/tile_check.v
@@ -1,19 +1,19 @@
 module tile_check(
-		output	[5:0]	tile_type,
-		output		end_signal,
-		input		start_signal,
-		input	[2:0]	up_tile,
-		input	[2:0]	down_tile,
-		input	[2:0]	right_tile,
-		input	[2:0]	left_tile,
-		input		clock
+		output reg [5:0]	tile_type,
+		output reg		endsignal,
+		input			start_signal,
+		input	[2:0]		up_tile,
+		input	[2:0]		down_tile,
+		input	[2:0]		right_tile,
+		input	[2:0]		left_tile,
+		input			clock
 		);
 
 reg white_input, black_input;	//number of white or black inputs into a tile
 reg empty_tile = 0 , left_white, up_white, right_white, down_white;
 //tiles are slash_down , slash_up , plus_vrt , plus_hz , baskslash_up , backsalsh_down , these are based on white color
-parameter slash_down , slash_up , plus_vrt , plus_hz , baskslash_up , backsalsh_down;
-parameter empty;
+parameter slash_down = 1, slash_up = 2, plus_vrt = 3, plus_hz = 4, backslash_up = 5, backslash_down = 6;
+parameter empty = 0;
 initial
 begin
 	// chek the white inputs
@@ -31,7 +31,7 @@ begin
 
 	if(right_tile == slash_up || right_tile == plus_hz || right_tile == backslash_down)
 	begin
-		white_input++;
+		white_input = white_input + 1;
 		right_white = 1;
 	end
 
@@ -73,19 +73,19 @@ begin
 		begin
 			if(up_white == 1)
 			begin
-				tile_type[slash_up] = 1;
+				tile_type[slash_up - 1] = 1;
 				endsignal = 1;
 			end
 
 			else if(right_white == 1)
 			begin
-				tile_type[plus_hz] = 1;
+				tile_type[plus_hz - 1] = 1;
 				endsignal = 1;
 			end
 			
 			else if (down_white == 1)
 			begin
-				tile_type[backslash_down] = 1;
+				tile_type[backslash_down - 1] = 1;
 				endsignal = 1;
 			end
 		end
@@ -94,13 +94,13 @@ begin
 		begin
 			if(right_white == 1)
 			begin
-				tile_type[backslash_up] = 1;
+				tile_type[backslash_up - 1] = 1;
 				endsignal = 1;
 			end
 			
 			if(down_white == 1)
 			begin
-				tile_type[plus_vrt] = 1;
+				tile_type[plus_vrt - 1] = 1;
 				endsignal = 1;
 			end
 		end
@@ -109,7 +109,7 @@ begin
 		begin
 			if(down_white == 1)
 			begin
-				tile_type[slash_down] = 1;
+				tile_type[slash_down - 1] = 1;
 				endsignal = 1;
 			end
 		end
@@ -121,21 +121,21 @@ begin
 	begin
 		if(left_white == 0)	//chek the left tile with others
 		begin
-			if(up_black == 0)
+			if(up_white == 1)
 			begin
-				tile_type[slash_up] = 1;
+				tile_type[slash_up - 1] = 1;
 				endsignal = 1;
 			end
 
 			else if(right_white == 0)
 			begin
-				tile_type[plus_hz] = 1;
+				tile_type[plus_hz - 1] = 1;
 				endsignal = 1;
 			end
 			
 			else if (down_white == 0)
 			begin
-				tile_type[backslash_down] = 1;
+				tile_type[backslash_down - 1] = 1;
 				endsignal = 1;
 			end
 		end
@@ -144,13 +144,13 @@ begin
 		begin
 			if(right_white == 0)
 			begin
-				tile_type[backslash_up] = 1;
+				tile_type[backslash_up - 1] = 1;
 				endsignal = 1;
 			end
 			
 			if(down_white == 0)
 			begin
-				tile_type[plus_vrt] = 1;
+				tile_type[plus_vrt - 1] = 1;
 				endsignal = 1;
 			end
 		end
@@ -159,101 +159,101 @@ begin
 		begin
 			if(down_white == 0)
 			begin
-				tile_type[slash_down] = 1;
+				tile_type[slash_down - 1] = 1;
 				endsignal = 1;
 			end
 		end
 	end	//end of checking mandatory moves for black
 
 	//check if left tile is the only one set before(in the 4 tile up, down, left, right)
-	if(left_tile != empty && up_tile == empty && right_tile == empty && down_tile == empty)
+	if(left_tile != empty && up_tile == empty && right_tile == empty && down_tile == empty && endsignal == 0)
 	begin
-		if(white_input = 1)
+		if(white_input == 1)
 		begin
-			tile_type[slash_up] = 1;
-			tile_type[plus_hz] = 1;
-			tile_type[backslash_down] = 1;
+			tile_type[slash_up - 1] = 1;
+			tile_type[plus_hz - 1] = 1;
+			tile_type[backslash_down - 1] = 1;
 			endsignal = 1;
 		end
 
 		else if(black_input == 1)
 		begin
-			tile_type[slash_down] = 1;
-			tile_type[plus_vrt] = 1;
-			tile_type[backslash_up] = 1;
+			tile_type[slash_down - 1] = 1;
+			tile_type[plus_vrt - 1] = 1;
+			tile_type[backslash_up - 1] = 1;
 			endsignal = 1;
 		end
 	end
 
 	//check if up tile is the only one set before(in the 4 tile up, down, left, right)
-	if(left_tile == empty && up_tile != empty && right_tile == empty && down_tile == empty)
+	if(left_tile == empty && up_tile != empty && right_tile == empty && down_tile == empty && endsignal == 0)
 	begin
-		if(white_input = 1)
+		if(white_input == 1)
 		begin
-			tile_type[slash_up] = 1;
-			tile_type[plus_vrt] = 1;
-			tile_type[backslash_up] = 1;
+			tile_type[slash_up - 1] = 1;
+			tile_type[plus_vrt - 1] = 1;
+			tile_type[backslash_up - 1] = 1;
 			endsignal = 1;
 		end
 
 		else if(black_input == 1)
 		begin
-			tile_type[slash_down] = 1;
-			tile_type[plus_hz] = 1;
-			tile_type[backslash_down] = 1;
+			tile_type[slash_down - 1] = 1;
+			tile_type[plus_hz - 1] = 1;
+			tile_type[backslash_down - 1] = 1;
 			endsignal = 1;
 		end
 	end
 
 	//check if right tile is the only one set before(in the 4 tile up, down, left, right)
-	if(left_tile == empty && up_tile == empty && right_tile != empty && down_tile == empty)
+	if(left_tile == empty && up_tile == empty && right_tile != empty && down_tile == empty && endsignal == 0)
 	begin
-		if(white_input = 1)
+		if(white_input == 1)
 		begin
-			tile_type[slash_down] = 1;
-			tile_type[plus_hz] = 1;
-			tile_type[backslash_up] = 1;
+			tile_type[slash_down - 1] = 1;
+			tile_type[plus_hz - 1] = 1;
+			tile_type[backslash_up - 1] = 1;
 			endsignal = 1;
 		end
 
 		else if(black_input == 1)
 		begin
-			tile_type[slash_up] = 1;
-			tile_type[plus_vrt] = 1;
-			tile_type[backslash_down] = 1;
+			tile_type[slash_up - 1] = 1;
+			tile_type[plus_vrt - 1] = 1;
+			tile_type[backslash_down - 1] = 1;
 			endsignal = 1;
 		end
 	end
 
 	//check if down tile is the only one set before(in the 4 tile up, down, left, right)
-	if(left_tile == empty && up_tile == empty && right_tile == empty && down_tile != empty)
+	if(left_tile == empty && up_tile == empty && right_tile == empty && down_tile != empty && endsignal == 0)
 	begin
-		if(white_input = 1)
+		if(white_input == 1)
 		begin
-			tile_type[slash_down] = 1;
-			tile_type[plus_vrt] = 1;
-			tile_type[backslash_down] = 1;
+			tile_type[slash_down - 1] = 1;
+			tile_type[plus_vrt - 1] = 1;
+			tile_type[backslash_down - 1] = 1;
 			endsignal = 1;
 		end
 
 		else if(black_input == 1)
 		begin
-			tile_type[slash_up] = 1;
-			tile_type[plus_hz] = 1;
-			tile_type[backslash_up] = 1;
+			tile_type[slash_up - 1] = 1;
+			tile_type[plus_hz - 1] = 1;
+			tile_type[backslash_up - 1] = 1;
 			endsignal = 1;
 		end
 	end
 
 	//if the left and the up tile is set before
-	if(left_tile != empty && up_tile != empty && right_tile == empty && down_tile == empty)
+	if(left_tile != empty && up_tile != empty && right_tile == empty && down_tile == empty && endsignal == 0)
 	begin
 		if(left_white == 1)
 		begin
 			if(up_white == 0)
 			begin
-				tile_type[plus_hz] = 1;
-				tile_type[backslash_down] = 1;
+				tile_type[plus_hz - 1] = 1;
+				tile_type[backslash_down - 1] = 1;
 				endsignal = 1;
 			end
 		end
@@ -261,22 +261,22 @@ begin
 		begin
 			if(up_white == 1)
 			begin
-				tile_type[plus_vrt] = 1;
-				tile_type[backslash_up] = 1;
+				tile_type[plus_vrt - 1] = 1;
+				tile_type[backslash_up - 1] = 1;
 				endsignal = 1;
 			end
 		end
 	end
 
 	//if the left and the right tile is set before
-	if(left_tile != empty && up_tile == empty && right_tile != empty && down_tile == empty)
+	if(left_tile != empty && up_tile == empty && right_tile != empty && down_tile == empty && endsignal == 0)
 	begin
 		if(left_white == 1)
 		begin
 			if(right_white == 0)
 			begin
-				tile_type[slash_up] = 1;
-				tile_type[backslash_down] = 1;
+				tile_type[slash_up - 1] = 1;
+				tile_type[backslash_down - 1] = 1;
 				endsignal = 1;
 			end
 		end
@@ -284,22 +284,22 @@ begin
 		begin
 			if(right_white == 1)
 			begin
-				tile_type[slash_down] = 1;
-				tile_type[backslash_up] = 1;
+				tile_type[slash_down - 1] = 1;
+				tile_type[backslash_up - 1] = 1;
 				endsignal = 1;
 			end
 		end
 	end
 
 	//if the left and the down tile is set before
-	if(left_tile != empty && up_tile == empty && right_tile == empty && down_tile != empty)
+	if(left_tile != empty && up_tile == empty && right_tile == empty && down_tile != empty && endsignal == 0)
 	begin
 		if(left_white == 1)
 		begin
 			if(down_white == 0)
 			begin
-				tile_type[slash_up] = 1;
-				tile_type[plus_hz] = 1;
+				tile_type[slash_up - 1] = 1;
+				tile_type[plus_hz - 1] = 1;
 				endsignal = 1;
 			end
 		end
@@ -307,22 +307,22 @@ begin
 		begin
 			if(down_white == 1)
 			begin
-				tile_type[slash_down] = 1;
-				tile_type[plus_vrt] = 1;
+				tile_type[slash_down - 1] = 1;
+				tile_type[plus_vrt - 1] = 1;
 				endsignal = 1;
 			end
 		end
 	end
 
 	//if the up and the right tile is set before
-	if(left_tile == empty && up_tile != empty && right_tile != empty && down_tile == empty)
+	if(left_tile == empty && up_tile != empty && right_tile != empty && down_tile == empty && endsignal == 0)
 	begin
 		if(up_white == 1)
 		begin
 			if(right_white == 0)
 			begin
-				tile_type[slash_up] = 1;
-				tile_type[plus_vrt] = 1;
+				tile_type[slash_up - 1] = 1;
+				tile_type[plus_vrt - 1] = 1;
 				endsignal = 1;
 			end
 		end
@@ -330,22 +330,22 @@ begin
 		begin
 			if(right_white == 1)
 			begin
-				tile_type[slash_down] = 1;
-				tile_type[plus_hz] = 1;
+				tile_type[slash_down - 1] = 1;
+				tile_type[plus_hz - 1] = 1;
 				endsignal = 1;
 			end
 		end
 	end
 
 	//if the up and the down tile is set before
-	if(left_tile == empty && up_tile != empty && right_tile == empty && down_tile != empty)
+	if(left_tile == empty && up_tile != empty && right_tile == empty && down_tile != empty && endsignal == 0)
 	begin
 		if(up_white == 1)
 		begin
 			if(down_white == 0)
 			begin
-				tile_type[slash_up] = 1;
-				tile_type[backslash_up] = 1;
+				tile_type[slash_up - 1] = 1;
+				tile_type[backslash_up - 1] = 1;
 				endsignal = 1;
 			end
 		end
@@ -353,22 +353,22 @@ begin
 		begin
 			if(down_white == 1)
 			begin
-				tile_type[slash_down] = 1;
-				tile_type[backslash_down] = 1;
+				tile_type[slash_down - 1] = 1;
+				tile_type[backslash_down - 1] = 1;
 				endsignal = 1;
 			end
 		end
 	end
 
 	//if the right and the down tile is set before
-	if(left_tile == empty && up_tile == empty && right_tile != empty && down_tile != empty)
+	if(left_tile == empty && up_tile == empty && right_tile != empty && down_tile != empty && endsignal == 0)
 	begin
 		if(right_white == 1)
 		begin
 			if(down_white == 0)
 			begin
-				tile_type[plus_hz] = 1;
-				tile_type[backslash_up] = 1;
+				tile_type[plus_hz - 1] = 1;
+				tile_type[backslash_up - 1] = 1;
 				endsignal = 1;
 			end
 		end
@@ -376,10 +376,11 @@ begin
 		begin
 			if(down_white == 1)
 			begin
-				tile_type[plus_vrt] = 1;
-				tile_type[backslash_down] = 1;
+				tile_type[plus_vrt - 1] = 1;
+				tile_type[backslash_down - 1] = 1;
 				endsignal = 1;
 			end
 		end
 	end
+end
 endmodule
