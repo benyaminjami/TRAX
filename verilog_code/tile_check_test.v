c8d126e359be9415ecdd88f106f1b503277035e2
mehdiz98 <mehdi_razavi97@yahoo.com>
2017-06-23 15:08:58 +0430

    Update tile_check_test.v

--- verilog_code/tile_check_test.v
+++ verilog_code/tile_check_test.v
@@ -6,18 +6,19 @@ wire endsignal;
 reg [2:0] left_tile, up_tile, right_tile, down_tile;
 
 reg start_signal = 0;
-tile_check t1(tile_type,endsignal,start_signal , up_tile, down_tile, right_tile, left_tile, );
+tile_check t1(tile_type, endsignal ,start_signal , up_tile, down_tile, right_tile, left_tile, );
 
 initial
 begin
 	up_tile = 3'b001;	left_tile = 3'b000;	right_tile = 3'b000;	down_tile = 3'b000;
-start_signal = 1; #5 start_signal = 0;
+start_signal = 1;
+
 #15	up_tile = 3'b001;	left_tile = 3'b000;	right_tile = 3'b000;	down_tile = 3'b010;
+#5 start_signal = 0;
+#5 start_signal = 1;
 #15    	up_tile = 3'b000;	left_tile = 3'b011;	right_tile = 3'b110;	down_tile = 3'b000;	
 end
 endmodule
-
-
 
 
 
